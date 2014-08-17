1a3030e7d7b4845bcc036fd9afc5d4e2572b5c12
Aram Kocharyan <akarmenia@gmail.com>
2014-08-17 17:38:46 +1000

    Added Meteor package file.

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  summary: "A collection of utility modules"
+});
+
+Package.on_use(function(api) {
+  api.export([
+    'Booleans', 'Collections', 'Objects', 'Setter', 'Strings', 'Types'
+  ], ['client', 'server']);
+  api.export([
+    'Forms', 'Window'
+  ], 'client');
+  api.add_files([
+    'Booleans.coffee', 'Collections.coffee', 'Objects.coffee', 'Setter.coffee',
+    'Strings.coffee', 'Types.coffee'
+  ], ['client', 'server']);
+  api.add_files([
+    'Forms.coffee', 'Window.coffee'
+  ], 'client');
+});
