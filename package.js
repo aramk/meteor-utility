1a9476e65adc0f6d49bf0eeee3c50e85acef5b08
Aram Kocharyan <akarmenia@gmail.com>
2014-10-07 12:28:39 +1100

    Fixed dependencies, added version and git.

--- package.js
+++ package.js
@@ -1,11 +1,13 @@
 Package.describe({
   name: 'aramk:utility',
-  summary: 'A collection of utility modules'
+  summary: 'A collection of utility modules',
+  version: '0.1.0',
+  git: 'https://github.com/aramk/meteor-utility.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use(['coffeescript', 'underscore', 'stevezhu:lodash'], ['client', 'server']);
+  api.use(['coffeescript', 'underscore', 'stevezhu:lodash@0.2.0'], ['client', 'server']);
   api.use(['deps', 'templating', 'jquery'], 'client');
   api.use(['aldeed:autoform', 'mrt:moment'], 'client', {weak: true});
   api.export([
