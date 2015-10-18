94b1e304b20513126a5b0f5e26d3b09ecd3eedec
Aram Kocharyan <akarmenia@gmail.com>
2015-10-18 16:40:36 +1100

    Version 0.11.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aramk:utility',
   summary: 'A collection of utility modules',
-  version: '0.10.0',
+  version: '0.11.0',
   git: 'https://github.com/aramk/meteor-utility.git'
 });
 
